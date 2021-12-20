4d19a2e4d449c31a2e4761f171b8fd84b835f6c4
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2021-12-20 16:11:41 +0300

    add cors

--- index.py
+++ index.py
@@ -1,9 +1,22 @@
 #import statements
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+#our react frontend
+client_app = ['http://localhost:3000']
 
 #create app
 app = FastAPI()
 
 #register router
 app.include_router(student_router)
+
+#register app with CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = client_app,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
